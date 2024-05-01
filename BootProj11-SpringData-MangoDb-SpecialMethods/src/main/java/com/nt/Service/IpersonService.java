5b0767d2df4658a913f9b9ca06f56fbc81b4a3de
HUSSAIN SA <HUSSAIN SA@LAPTOP-KFSS52AN>
2024-05-01 16:48:49 +0530

    new details

---
+++ BootProj11-SpringData-MangoDb-SpecialMethods/src/main/java/com/nt/Service/IpersonService.java
@@ -0,0 +1,9 @@
+package com.nt.Service;
+
+import java.util.List;
+
+import com.nt.Document.PersonDetails;
+
+public interface IpersonService {
+	List<PersonDetails>insertDetails(PersonDetails details);
+}
---
+++ BootProj11-SpringData-MangoDb-SpecialMethods/src/main/java/com/nt/Service/PersonServiceImpl.java
@@ -0,0 +1,8 @@
+package com.nt.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service("PersonServiceImpl")
+public interface PersonServiceImpl extends IpersonService {
+
+}
