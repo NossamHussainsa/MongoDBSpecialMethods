c720860edadff2c941cf7c8e24ac3f0b9ce6c584
HUSSAIN SA <HUSSAIN SA@LAPTOP-KFSS52AN>
2024-05-01 16:49:55 +0530

    chaning interface to class

--- BootProj11-SpringData-MangoDb-SpecialMethods/src/main/java/com/nt/Service/PersonServiceImpl.java
+++ BootProj11-SpringData-MangoDb-SpecialMethods/src/main/java/com/nt/Service/PersonServiceImpl.java
@@ -1,8 +1,18 @@
 package com.nt.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.nt.Document.PersonDetails;
+
 @Service("PersonServiceImpl")
-public interface PersonServiceImpl extends IpersonService {
+public class PersonServiceImpl implements IpersonService {
+
+	@Override
+	public List<PersonDetails> insertDetails(PersonDetails details) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
